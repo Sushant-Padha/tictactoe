a71a1ea6f372c2c4cc1caa0eb3e1463e3816ea29
sushant <sushant.padha@gmail.com>
2021-03-08 12:01:22 +0530

    added get_value method ; updated docstrings

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -8,6 +8,8 @@ box_drawing = ['┛', '┫', '┓', '┳', '┏', '┣', '┗', '┻', '━', '�
 class Board:
     '''Defines new board to play tictactoe
     '''
+    # list of all prior updates to board state
+    updates = []
 
     def __init__(self, n: int = 3, value: str = ''):
         '''Instantiate new board instance
@@ -17,6 +19,7 @@ class Board:
             value (str): value to fill the board with
         '''
         self.n = n
+        self.value = value
         self.state = [[value for _ in range(n)].copy() for _ in range(n)]
 
     def __str__(self):
@@ -35,12 +38,16 @@ class Board:
         Args:
             index (int): index of position to update
             value (str): value to update to
+
+        Raises:
+            IndexError: invalid index for board
         '''
         i1, i2 = self.convert_index(index)
         try:
             self.state[i1][i2] = value
         except:
-            raise IndexError('Invalid index')
+            raise IndexError('Invalid index for board')
+        self.updates.append(index)
 
     @staticmethod
     def convert_index(index: int):
@@ -55,3 +62,19 @@ class Board:
         i1 = index // 3
         i2 = index % 3
         return i1, i2
+
+    def get_value(self, index: int):
+        '''Get the value at index
+
+        Args:
+            index (int): index
+
+        Raises:
+            IndexError: invalid index for board
+        '''
+        i1, i2 = self.convert_index(index)
+        try:
+            value = self.state[i1][i2]
+        except:
+            raise IndexError('Invalid index for board')
+        return value
