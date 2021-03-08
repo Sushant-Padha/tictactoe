503c7a55c6edc1dceecfb0e10b5518e918ac9aa0
sushant <sushant.padha@gmail.com>
2021-03-08 12:02:39 +0530

    updated import

--- tictactoe/test/test_board.py
+++ tictactoe/test/test_board.py
@@ -1,4 +1,4 @@
-from .. import board
+from tictactoe.board import Board
 import pytest
 
 n = 3
@@ -6,7 +6,7 @@ default_value = 'X'
 board_state = [['X', 'X', 'X'],
                ['X', 'X', 'X'],
                ['X', 'X', 'X']]
-board_obj = board.Board(n=n, value=default_value)
+board_obj = Board(n=n, value=default_value)
 index = 6  # 2, 0
 i1, i2 = 2, 0
 value = 'O'
@@ -32,13 +32,13 @@ def test_update():
     new_board_state = [['X', 'X', 'X'],
                        ['X', 'X', 'X'],
                        ['O', 'X', 'X']]
-    new_board_obj = board.Board(n=n, value=default_value)
+    new_board_obj = Board(n=n, value=default_value)
     new_board_obj.update(6, value)
     assert new_board_obj.state == new_board_state
 
 
 def test_update_exception():
-    new_board_obj = board.Board(n=n, value=default_value)
+    new_board_obj = Board(n=n, value=default_value)
     try:
         new_board_obj.update(9, value)  # should raise IndexError
     except:
