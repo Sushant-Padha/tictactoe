6847c30a2795e55f0a732113dc564c01d158b25c
sushant <sushant.padha@gmail.com>
2021-03-11 20:17:54 +0530

    added console.py for CLI

---
+++ tictactoe/console.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+
+class Console:
+    '''Defines new Console object for I/O
+    '''
+
+    def __init__(self):
+        '''Instantiate new Console instance
+        '''
+        pass
+
+    @staticmethod
+    def clear():
+        os.system('cls')
+
+    def print(*args, **kwargs):
+        return print(*args, **kwargs)
+
+    def input(*args, **kwargs):
+        return input(*args, **kwargs)
