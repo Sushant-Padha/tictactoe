cccb6769c8864a6e0ab100e783513ce74cac9bff
sushant <sushant.padha@gmail.com>
2021-03-13 13:56:56 +0530

    changed I/O messages

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -22,10 +22,10 @@ def main():
     player1 = Player(marker=marker1, name=name1, board=board)
     player2 = Player(marker=marker2, name=name2, board=board)
     game = Game(player1=player1, player2=player2, board=board, console=console)
-    input("The game begins! Good luck. [Press Enter to start]")
+    input("\nThe game begins! Good luck. [Press Enter to start]")
     console.clear()
     winner = game.play()
-    print(f"Congratulations '{winner.name}'! " +
+    print(f"\nCongratulations '{winner.name}'! " +
           "You have won the game.")
     print("Thanks for playing the game!")
     play_again = input("Would you like to play again? " +
@@ -37,10 +37,10 @@ def main():
 
 
 def print_instructions():
-    print("\tTo select a unit for making a move, write its index," +
+    print("\tTo select a unit for making a move, write its index, " +
           "starting from 0 in the top left corner, increasing by one " +
           "as we go to the next column and skip to the beginning of the " +
-          "next row.")
+          "next row.\n")
 
 
 if __name__ == '__main__':
