2709ff8351345571daca036174186350b5267c3b
sushant <sushant.padha@gmail.com>
2021-03-16 17:48:51 +0530

    add more info, requirements in `setup.py`

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
 from setuptools import setup, find_packages
 
-setup(name='tictactoe', version='1.0', packages=find_packages())
+requirements_file = './requirements.txt'
+
+with open(requirements_file, 'r') as f:
+    install_requires = [
+        line for line in f.read().splitlines() if len(line) > 0]
+
+setup(
+    name='tictactoe',
+    description='simple command line tictactoe game',
+    author='sushant',
+    author_email='sushant.padha@gmail.com',
+    version='1.0',
+    url='https://github.com/Sushant-Padha/tictactoe',
+    license='MIT License',
+    packages=find_packages(),
+    install_requires=install_requires
+)
