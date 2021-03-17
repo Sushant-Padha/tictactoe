2c59a5342edaf4c4c56a6a7c182b5ec727f3758a
sushant <sushant.padha@gmail.com>
2021-03-17 11:36:52 +0530

    changed setup install_requires

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_packages
 
-requirements_file = './requirements.txt'
-
-with open(requirements_file, 'r') as f:
-    install_requires = [
-        line for line in f.read().splitlines() if len(line) > 0]
-
 setup(
     name='tictactoe',
     description='simple command line tictactoe game',
@@ -14,6 +8,5 @@ setup(
     version='1.0',
     url='https://github.com/Sushant-Padha/tictactoe',
     license='MIT License',
-    packages=find_packages(),
-    install_requires=install_requires
+    packages=find_packages()
 )
